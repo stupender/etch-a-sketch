03d294a69feb637e2cbed41542275f4222b3b791
Stu <stu1992@gmail.com>
2021-01-26 12:48:15 -0600

    grid-template-updates-slider

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // VARIABLES TO HOLD CANVAS, SLIDER
 let canvas = document.querySelector('#canvas');
-let input = document.querySelector('#slider');
 let clear = document.querySelector("#clear");
+let slider = document.getElementById("slider");
 
 // FUNCTION TO GENERATE DEFAULT GRID
-function createDefaultGrid(number) {
+function createGrid(value) {
     // LOOP FOR 16 x 16 PIXEL GRID
-    for (let i = 0; i < number * number; i++) {
+    for (let i = 0; i < value * value; i++) {
         let div = document.createElement('div');
         div.classList.add('blank');
+        canvas.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+        canvas.style.gridTemplateRows = `repeat(${value}, 1fr)`;
         canvas.appendChild(div);
         // EVENT LISTENER TO TURN MOUSEOVER BOXES BLACK
         div.addEventListener('mouseover', function(event) {
@@ -24,18 +26,17 @@ function createDefaultGrid(number) {
     }
 }
 
-createDefaultGrid(16);
+// CALL FUNCTION TO CREATE GRID WITH CURRENT (DEFAULT) SLIDER VALUE
+let sliderValue = parseInt(slider.value, 10);
+createGrid(sliderValue);
 
 // UPDATE DISPLAY TEXT TO MATCH SLIDER VALUE w/ EVENT LISTENER
-let slider = document.getElementById("slider");
-let sliderValue = parseInt(slider.value, 10);
-
 slider.addEventListener("mousemove", function () { 
     document.getElementById('displayValue').textContent = "Size: " + slider.value;
 }); 
 
 // UPDATE GRID SIZE TO MATCH SLIDER VALUE w/ EVENT LISTENER -- this doesn't seem to be doing the trick yet.
-
 slider.addEventListener("mouseup", function () {
-    createDefaultGrid(sliderValue);
+    let sliderValue = parseInt(slider.value, 10);
+    createGrid(sliderValue);
 });
